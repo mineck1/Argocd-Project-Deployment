add91eb95c8f1ac868a9bd9c6030fd9f7d92ae7b
ckmine <36189229+ckmine@users.noreply.github.com>
2022-12-01 14:19:39 +0530

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+PORT=$(kubectl -n test get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
 # first run this
 chmod 777 $(pwd)
