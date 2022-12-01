5fa266b52b994ad89da7c6465507d6c8d39c8537
ckmine <36189229+ckmine@users.noreply.github.com>
2022-12-01 12:31:40 +0530

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -2,11 +2,11 @@
 sed -i "s#replace#${imageName}#g" blue.yml
 kubectl -n test get deployment ${deploymentName} > /dev/null
 
- if [[ $? -ne 0 ]]; then
-     echo "deployment ${deploymentName} doesnt exist"
-   kubectl -n  apply -f blue.yml
-    echo "deployment ${deploymentName} exist"
-    echo "image name - ${imageName}"
-    kubectl -n test set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
-fi
-#kubectl  apply -f blue.yml
+# if [[ $? -ne 0 ]]; then
+  #   echo "deployment ${deploymentName} doesnt exist"
+  # kubectl -n  apply -f blue.yml
+  #  echo "deployment ${deploymentName} exist"
+  #  echo "image name - ${imageName}"
+   # kubectl -n test set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
+#fi
+kubectl  apply -f blue.yml
